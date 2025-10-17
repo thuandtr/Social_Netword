9250dcc4ac4a48db1bc37e3cce10e86b46a8a97b
dtr_thuann <102339067+ThuanTheBadLuckCoder@users.noreply.github.com>
2025-10-17 16:45:27 +0700

    small setting up
    that's all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello from Indian Coders ✨");
+import app from "./app";
+import { config } from "dotenv";
+
+config();
+
+const init = () => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log("Server is running on port: " + PORT);
+    });
+}
+
+init();
